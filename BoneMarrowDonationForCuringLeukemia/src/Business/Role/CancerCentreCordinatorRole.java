32af5b34e8a67f0b7af9d0561c1065cf24ba5dd6
Balu <mundlamuri.b@northeastern.edu>
2021-12-08 22:09:07 -0500

    Required classes created

--- BoneMarrowDonationForCuringLeukemia/src/Business/Role/CancerCentreCordinatorRole.java
+++ BoneMarrowDonationForCuringLeukemia/src/Business/Role/CancerCentreCordinatorRole.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Business.Network;
+package Business.Role;
 
 /**
  *
  * @author balumullamuri
  */
-public class Network {
+public class CancerCentreCordinatorRole {
     
 }

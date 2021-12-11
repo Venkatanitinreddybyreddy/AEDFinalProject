7cbf5650e286de63f9cbbf224e8753788d3d4f06
Venkatanitinreddybyreddy <byreddy.v@northeastern.edu>
2021-12-11 00:42:49 -0500

    Work Queue,user account methods and objects

--- BoneMarrowDonationForCuringLeukemia/src/Magic/Design/MyJButton.java
+++ BoneMarrowDonationForCuringLeukemia/src/Magic/Design/MyJButton.java
@@ -4,10 +4,23 @@
  */
 package Magic.Design;
 
+import java.awt.Color;
+
 /**
  *
  * @author balumullamuri
  */
-public class MyJButton {
+public class MyJButton extends javax.swing.JButton {
+    public MyJButton() {
+        designMyButton();
+    }
+    
+    private void designMyButton() {
+//        this.setBorder(new javax.swing.border.MatteBorder(null));
+//        this.setBackground(new Color(31, 31, 31));
+        this.setForeground(new Color(0,0,0));
+//        this.setFont(new java.awt.Font("Tahoma", 0, 11));
+//        this.setSize(50,50);
+    }
     
 }

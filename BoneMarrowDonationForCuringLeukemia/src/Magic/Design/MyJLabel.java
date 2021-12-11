a35bba1ba1987324bbe9f89795f3d4e0fbabf20d
Venkatanitinreddybyreddy <byreddy.v@northeastern.edu>
2021-12-11 00:51:49 -0500

    design class files

--- BoneMarrowDonationForCuringLeukemia/src/Magic/Design/MyJLabel.java
+++ BoneMarrowDonationForCuringLeukemia/src/Magic/Design/MyJLabel.java
@@ -8,6 +8,25 @@ package Magic.Design;
  *
  * @author balumullamuri
  */
-public class MyJLabel {
+public class MyJLabel extends javax.swing.JLabel {
+      public MyJLabel() {
+        
+        setMagicDesign();
+    }
+
+    private void setMagicDesign() {
+        
+//        this.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
+
+        this.setForeground(new java.awt.Color(0, 0, 0));
+        
+        this.setBackground(new java.awt.Color(255, 255, 255));
+
+        this.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+
+        this.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+
+
+    }
     
 }

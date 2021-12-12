97bd41a5e438014bda2aaa20e1541dfea3b1c6a7
Balu <mundlamuri.b@northeastern.edu>
2021-12-12 11:44:05 -0500

    Update private.xml

--- BoneMarrowDonationForCuringLeukemia/src/Business/Network/Network.java
+++ BoneMarrowDonationForCuringLeukemia/src/Business/Network/Network.java
@@ -4,10 +4,58 @@
  */
 package Business.Network;
 
+import Business.Enterprise.EnterpriseDirectory;
+
 /**
  *
  * @author balumullamuri
  */
 public class Network {
+    private String name;
+    private String state;
+    private String country;
+    
+    
+    
+    private EnterpriseDirectory enterpriseDirectory;
+    
+    public Network(){
+        enterpriseDirectory=new EnterpriseDirectory();
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public EnterpriseDirectory getEnterpriseDirectory() {
+        return enterpriseDirectory;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+    
+    
+    
+    
+    @Override
+    public String toString(){
+        return name;
+    }
     
 }
